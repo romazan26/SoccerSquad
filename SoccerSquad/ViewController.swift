49f09eaf1052f43e5c890ced1fd59420969e38c3
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-07-08 21:35:15 +0300

    Initial Commit

---
+++ SoccerSquad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoccerSquad
+//
+//  Created by Роман on 08.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
